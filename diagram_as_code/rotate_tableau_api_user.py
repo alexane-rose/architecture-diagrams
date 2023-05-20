61b3b5795ef4c553a223e7f934e9c20028851604
Alexane Rose <alexane.rose@nxp.com>
2023-05-20 13:40:15 +0200

    rotate tableau user credentials

---
+++ diagram_as_code/rotate_tableau_api_user.py
@@ -0,0 +1,39 @@
+from diagrams import Cluster
+from diagrams import Diagram
+from diagrams import Edge
+from diagrams.aws.compute import Lambda
+from diagrams.aws.analytics import Athena
+from diagrams.aws.management import Cloudwatch
+from diagrams.aws.management import CloudwatchEventTimeBased
+from diagrams.aws.integration import Eventbridge
+from diagrams.aws.security import SecretsManager
+from diagrams.aws.security import IdentityAndAccessManagementIam as IAM
+from diagrams.aws.general import GenericDatabase
+
+import os
+filename = os.path.basename(__file__).replace(".py","")
+
+graph_attr = {
+    "fontsize": "12",
+    "bgcolor": "white"
+}
+
+with Diagram(filename, show= False, direction="LR", outformat="png", filename = f"output/{filename}", graph_attr=graph_attr):
+    with Cluster("AWS") :
+        schedule_rotation = CloudwatchEventTimeBased("Rotation Schedule")
+        API_user = IAM('API User')
+        API_user_secret = SecretsManager("API User Secrets")
+        rotation_event = Eventbridge("Rotation Event")
+        athena = Athena("Athena")
+        lambda_fn = Lambda("Update Credentials")
+        tableau_user_secret = SecretsManager("Tableau User")
+
+        schedule_rotation >> API_user >> API_user_secret >> rotation_event >> lambda_fn
+        lambda_fn << Edge(style='dotted') << tableau_user_secret
+
+    with Cluster("Tableau"):
+        data_connection = GenericDatabase("Data Connection")
+
+    lambda_fn >> data_connection
+    data_connection >> athena
+    data_connection << Edge(style='dotted') << API_user_secret
--- makefile
+++ makefile
@@ -3,5 +3,5 @@ init :
 	pip install -r requirements.txt
 
 diagrams :
-	mkdir output
+	mkdir -p output
 	@for f in $(shell ls diagram_as_code); do python3 diagram_as_code/$${f}; done
